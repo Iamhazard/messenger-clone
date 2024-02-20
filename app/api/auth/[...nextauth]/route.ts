902a1b57fc192f542f7987b64271cb1b40b54f88
Amrit Acharya <Amrithazard@gmail.com>
2024-02-20 16:35:07 +0545

    ..

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -59,6 +59,7 @@ import prisma from "@/app/lib/prismadb"
   secret: process.env.NEXTAUTH_SECRET,
 }
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
+export default handler
